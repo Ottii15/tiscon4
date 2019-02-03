0aac3a2253a01303e2b04c9155e8635e6d5909d7
Ottii <82115039@ed.tus.ac.jp>
2019-02-03 14:37:05 +0900

    お客様情報登録画面

--- src/main/java/jp/co/tis/tiscon4/form/SelectForm.java
+++ src/main/java/jp/co/tis/tiscon4/form/SelectForm.java
@@ -12,7 +12,7 @@ import java.io.Serializable;
 
 
 
-public class IndexForm implements Serializable {
+public class SelectForm implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
